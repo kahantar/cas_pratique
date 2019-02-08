38c0f131aa79746febd3e984623f4a17fece4d4d
Karim AHANTAR <kahantar@e2r2p8.42.fr>
2019-02-08 02:14:35 +0100

    remove back and front

--- frontend/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { ReducerAllAlert, ReducerAllCustomer } from './allReducers';
-
-const rootReducer = combineReducers({
-    allAlert: ReducerAllAlert,
-    allClient: ReducerAllCustomer
-})
-
-export default rootReducer 
